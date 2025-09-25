a107afa13b91e77763ca0ce4bf5797842028a237
ahmed-mostafa2020 <ahmostafa054@gmail.com>
2025-09-25 22:38:14 +0300

    w s solving calender page issues3

--- src/components/sections/pages/calendar/Modal.jsx
+++ src/components/sections/pages/calendar/Modal.jsx
@@ -6,7 +6,7 @@ const Modal = ({ isOpen, onClose, children, maxWidth = "max-w-2xl" }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
       <div
-        className={`bg-white rounded-xl shadow-2xl ${maxWidth} w-full max-h-[90vh] overflow-y-auto`}
+        className={`p-6  bg-white rounded-xl shadow-2xl ${maxWidth} w-full max-h-[90vh] overflow-y-auto`}
       >
         {children}
       </div>
