280c7c98187d5892b119ce6359b47258aadf1071
mohamed-hegazy219427 <mohamed-hegazy219427@gmail.com>
2025-12-07 17:50:23 +0200

    fix: refactor SchoolOverviewCard for improved layout, readability, and functionality

--- src/components/sections/pages/profile/schoolManagementTeam/schoolsOverview/SchoolOverviewCard.jsx
+++ src/components/sections/pages/profile/schoolManagementTeam/schoolsOverview/SchoolOverviewCard.jsx
@@ -1,91 +1,85 @@
 "use client";
 
-import { Box, Typography, Button } from "@mui/material";
-import { useTranslations } from "next-intl";
+import { Box, Typography } from "@mui/material";
+import { useLocale, useTranslations } from "next-intl";
 import { bankSmall, locationGrayIcon, newSarSmall } from "@/src/assets/svg";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
+import formatCurrency from "@/src/utils/FormatCurrency";
+import { locale } from "dayjs";
 
 const SchoolOverviewCard = ({ item }) => {
   const t = useTranslations();
-  const router = useRouter();
+  const locale = useLocale();  
 
-  const handleClick = () => {
-    // dispatch(setOrganizationId(item._id));
-    router.push(
-      `/profile/school-team-management/schools-overview/schools-details/${item._id}`
-    );
-  };
   return (
-    <Box className="rounded-xl border-2 border-border px-4  py-6 shadow-sm bg-white  ">
+    <Box className="rounded-xl border-2 border-border px-4  py-6 shadow-sm bg-white flex flex-col gap-8  ">
       {/* Status Tag */}
       <Box className="flex justify-between mb-2">
         {/* Name + Icon */}
-        <Box className="flex justify-between items-center mb-1 ">
-          <Box className="flex items-center justify-center bg-[#13A1C166] px-3 py-1 w-12 h-12 rounded-lg me-2">
+        <Box className="flex justify-between items-center   mb-1 ">
+          <Box className="flex items-center justify-center self-stretch bg-[#13A1C166] px-3 py-1 w-20  rounded-lg me-2 ">
             <span className="block">{bankSmall}</span>
           </Box>
-          <Typography
-            variant="h6"
-            className="font-semibold !font-somar truncate"
-          >
-            {item.name} - {item.city}
-          </Typography>
-          <Box className="bg-[#00AB2B66] text-[#033440] px-4 py-2 rounded-full text-sm flex items-center justify-center gap-1">
-            ممتاز
+          <Box className=" flex flex-col flex-1  overflow-hidden">
+            <Typography
+              variant="h6"
+              className="font-semibold  !font-somar w-full  line-clamp-1 "
+            >
+              {item.name} - {item.city}
+            </Typography>
+            <Box className="flex items-center gap-1 text-gray-500 text-sm mb-3 ">
+              <span>{locationGrayIcon}</span>
+              <Typography className="!font-somara" variant="body1">
+                {item.city}
+              </Typography>
+            </Box>
           </Box>
         </Box>
-
-        <Box className="bg-[#00AB2B66] text-[#033440] px-4 py-2 rounded-full text-sm flex items-center justify-center gap-1">
+        <Box className="bg-[#00AB2B66] text-[#033440] max-w-fit h-fit  px-4 py-2 rounded-full shadow-[#033440]  text-sm flex items-center justify-center gap-1">
           ممتاز
         </Box>
       </Box>
 
       {/* City */}
-      <Box className="flex items-center gap-1 text-gray-500 text-sm mb-3 ">
-        <span>{locationGrayIcon}</span>
-        <Typography className="!font-somara" variant="body1">
-          {item.city}
-        </Typography>
-      </Box>
 
       {/* Revenue */}
-      <Box className="bg-[#4BFC4E33] px-4  rounded-md mb-3 flex justify-between py-4">
-        <Typography
-          variant="body"
-          className="text-gray-700 !font-medium !font-somara"
-        >
-          {t("profile.schools_overview.cards.total_revenue")}
-        </Typography>
-        <Typography
-          variant="body"
-          className="text-gray-800 font-bold !font-somara flex "
-        >
-          {item.totalRevenue.toLocaleString()}
-          <span className="ms-1">{newSarSmall}</span>
-        </Typography>
-      </Box>
+      <Box className="flex flex-col gap-3">
+        <Box className="bg-[#4BFC4E33] px-4  rounded-md  flex justify-between py-4">
+          <Typography
+            variant="body"
+            className="text-[#1E1E1C] !font-medium !font-somara"
+          >
+            {t("profile.schools_overview.cards.total_revenue")}
+          </Typography>
+          <Typography
+            variant="body"
+            className="text-[#1E1E1C] font-semibold !font-somara flex "
+          >
+            {formatCurrency(item.totalRevenue, locale)}
+          </Typography>
+        </Box>
 
-      {/* Students Count */}
-      <Box className="bg-[#4FDCFF33] px-4 py-4 rounded-md mb-3 text-center flex justify-between ">
-        <Typography
-          variant="body"
-          className="text-[#00707F] text-sm !font-somara"
-        >
-          {t("profile.schools_overview.cards.active_students")}
-        </Typography>
-        <Typography
-          variant="body"
-          className="text-[#0B7F8F] font-bold !font-somara"
-        >
-          {item.childsCount}
-        </Typography>
+        {/* Students Count */}
+        <Box className="bg-[#4FDCFF33] px-4 py-4 rounded-md  text-center flex justify-between ">
+          <Typography
+            variant="body"
+            className="text-[#00707F] text-sm !font-somara"
+          >
+            {t("profile.schools_overview.cards.active_students")}
+          </Typography>
+          <Typography
+            variant="body"
+            className="text-[#0B7F8F] font-bold !font-somara"
+          >
+            {item.childsCount}
+          </Typography>
+        </Box>
       </Box>
 
       {/* Trips Row */}
-      <Box className="grid grid-cols-3 gap-2 text-center mb-4">
+      <Box className="grid grid-cols-3 gap-4 text-center ">
         {/* Pending */}
-        <Box className="bg-[#2991AA14] py-2 rounded-lg flex flex-col">
+        <Box className="bg-[#2991AA14] shadow-[rgba(41,144,170,0.16)] py-2 rounded-lg flex flex-col">
           <Typography
             variant="body"
             className="text-[#2991AA] font-bold !font-somara"
@@ -101,7 +95,7 @@ const SchoolOverviewCard = ({ item }) => {
         </Box>
 
         {/* Completed */}
-        <Box className="bg-[#76A13329] py-2 rounded-lg flex flex-col">
+        <Box className="bg-[rgba(119,161,51,0.08)] shadow-[rgba(118,161,51,0.16)] py-2 rounded-lg flex flex-col">
           <Typography
             variant="body"
             className="text-[#76A133] font-bold !font-somara "
@@ -117,7 +111,7 @@ const SchoolOverviewCard = ({ item }) => {
         </Box>
 
         {/* In Progress */}
-        <Box className="bg=[#EB010114] py-2 rounded-lg flex flex-col">
+        <Box className="bg-[#EB010114] shadow-[rgba(235,1,1,0.16)] py-2 rounded-lg flex flex-col">
           <Typography
             variant="body"
             className="text-[#EB0101] !font-bold !font-somara"
@@ -134,7 +128,7 @@ const SchoolOverviewCard = ({ item }) => {
       </Box>
       {/* Button */}
       <Link
-        className="!font-somar font-semibold !bg-[#0B7F8F]  !py-4  !text-white  rounded-lg"
+        className="!font-somar font-semibold !bg-[#0B7F8F] px-8 py-4 block text-center w-full !text-white  rounded-lg"
         href={`/schools-details/${item.id}`}
       >
         {t("profile.schools_overview.cards.show_details")}
