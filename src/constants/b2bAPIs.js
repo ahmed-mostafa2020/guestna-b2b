80401025819fdc99aa60482f6530071892b437cb
ahmed-mostafa2020 <ahmostafa054@gmail.com>
2025-12-21 16:41:04 +0200

    try to make QA link

--- src/constants/b2bAPIs.js
+++ src/constants/b2bAPIs.js
@@ -62,7 +62,7 @@ export const B2B_END_POINTS = {
   FAVORITE: "favorite",
 
   PROFILE: {
-    INFORMATION: "auth/userInfo", //MAIN
+    INFORMATION: "auth/userInfo",
     INFO: "profile/users/management/info",
 
     HEADER_FILTER_BY_ORGANIZATION: "profile/organizations/hover",
