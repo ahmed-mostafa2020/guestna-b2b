94f535dc04adaec5f1170a9e9e333b5d9a852606
Mohamed Hegazy <48334725+mohamed-hegazy219427@users.noreply.github.com>
2026-04-13 15:58:33 +0200

    fix : remove vaildation from excel bulk user template

--- src/constants/excelHeaders.js
+++ src/constants/excelHeaders.js
@@ -4,13 +4,6 @@ export const usersHeaders = ({ roles = [], locale = "en" }) => {
       key: "name",
       label: { ar: "الاسم", en: "Name" },
       width: 20,
-      validation: {
-        type: "text",
-        range: [2, 100],
-        required: true,
-        message: { ar: "يرجى التحقق من الاسم", en: "Please check your name" },
-        errorTitle: { ar: "الاسم غير صحيح", en: "Invalid name" },
-      },
     },
     {
       key: "email",
@@ -29,26 +22,6 @@ export const usersHeaders = ({ roles = [], locale = "en" }) => {
       key: "phone",
       label: { ar: "رقم الجوال", en: "Phone" },
       style: { numFmt: "@" }, // Force text
-      validation: {
-        type: "customFormula",
-        required: true,
-        formula: (colLetter, row) => `
-          AND(
-            ISTEXT(${colLetter}${row}),
-            LEN(SUBSTITUTE(${colLetter}${row}," ",""))>=8,
-            ISNUMBER(VALUE(SUBSTITUTE(${colLetter}${row},"+",""))),
-            OR(
-              LEFT(SUBSTITUTE(${colLetter}${row}," ",""),1)="+",
-              LEFT(SUBSTITUTE(${colLetter}${row}," ",""),1)="0"
-            )
-          )
-        `,
-        errorTitle: { ar: "رقم الجوال غير صحيح", en: "Invalid phone number" },
-        message: {
-          ar: "رقم الجوال يجب أن يكون رقم صحيح",
-          en: "Phone number must be valid",
-        },
-      },
     },
     {
       key: "role",
