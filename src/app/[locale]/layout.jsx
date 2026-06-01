41298a7f4f3a3231266a72b20884734c81c52655
ahmed-mostafa2020 <ahmostafa054@gmail.com>
2026-06-01 15:51:04 +0300

    deleted polyfill

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -229,11 +229,6 @@ export default async function RootLayout({ children, params: { locale } }) {
           defer
         ></Script>
 
-        <Script
-          src="https://polyfill.io/v3/polyfill.min.js?features=fetch"
-          async
-        ></Script>
-
         {/* PWA - iOS support */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
